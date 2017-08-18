9796b4c7806e5ab7c50fcecb881d40f3b6df0f9e
Kamil <kamil.kindziuk@gmail.com>
2017-08-18 07:49:07 +0200

    add Vehicle class

---
+++ src/vehicle.cpp
@@ -0,0 +1,5 @@
+#include "vehicle.h"
+
+Vehicle::Vehicle(const int i){
+  this->id = i;
+}
---
+++ src/vehicle.h
@@ -0,0 +1,19 @@
+#ifndef VEHICLE
+#define VEHICLE
+
+class Vehicle {
+
+  public:
+    
+    int id;
+    double s;
+    double d;
+    double vx;
+    double vy;
+    double v;
+
+    Vehicle(const int i);
+
+};
+
+#endif /* VEHICLE */
