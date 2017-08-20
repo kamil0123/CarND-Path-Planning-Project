aa48b898d4683e1a9a478530ee0129b022f3a484
Kamil <kamil.kindziuk@gmail.com>
2017-08-20 15:18:13 +0200

    create Behavior class

---
+++ src/behavior.h
@@ -0,0 +1,14 @@
+#ifndef BEHAVIOR_H_
+#define BEHAVIOR_H_
+
+class Behavior {
+
+  public:
+
+    BehaviorAccType accType;
+    BehaviorLaneType laneType;
+
+    Behavior();
+};
+
+#endif /* BEHAVIOR_H_ */
