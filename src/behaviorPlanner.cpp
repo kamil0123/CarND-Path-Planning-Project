08d4e9f53f644bb588a9b00728c19334ad37996d
Kamil <kamil.kindziuk@gmail.com>
2017-08-21 18:42:14 +0200

    implement BehaviorPlanner::updateState

--- src/behaviorPlanner.cpp
+++ src/behaviorPlanner.cpp
@@ -13,5 +13,34 @@ Behavior BehaviorPlanner::updateState(Vehicle& car, std::vector<Vehicle>& otherV
 	currentLaneState.update(car, otherVehicles, car.lane);
 	leftLaneState.update(car, otherVehicles, car.laneOnLeft);
 	rightLaneState.update(car, otherVehicles, car.laneOnRight);
-	
+
+  Behavior behavior;
+  
+  bool too_close = false;
+  double too_close_speed = 0.0;
+  // check there is a car in front of us and closer than 30 meters
+  if (currentLaneState.front_distance < 30) {
+    too_close = true;
+    too_close_speed = currentLaneState.front_s;
+    
+    if (car.lane == Lane::CENTER) {
+      behavior.laneType = BehaviorLaneType::LANE_CHANGE_LEFT;
+    } else if (car.lane == Lane::LEFT) {
+      behavior.laneType = BehaviorLaneType::LANE_CHANGE_RIGHT;
+    } else {
+      behavior.laneType = BehaviorLaneType::KEEP_LANE;
+    }
+  } else {
+    behavior.laneType = BehaviorLaneType::KEEP_LANE;
+  }
+  
+  if (too_close && car.s > too_close_speed) {
+    behavior.accType = BehaviorAccType::DOWN;
+  } else if (car.s < 49.5) {
+    behavior.accType = BehaviorAccType::UP;
+  } else {
+    behavior.accType = BehaviorAccType::NONE;
+  }
+  
+  return behavior;
 }
